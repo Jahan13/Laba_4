2422b9da416680969d2693ea64b37e92a6747716
Jahan Ovezova <j4hanovezova@yandex.ru>
2020-12-07 23:46:48 +0300

    ADDED:helper-methods for converting coordinates to a point

--- src/bsu/rfct/java/Laba_4/GraphicsDisplay.java
+++ src/bsu/rfct/java/Laba_4/GraphicsDisplay.java
@@ -1,6 +1,7 @@
 package bsu.rfct.java.Laba_4;
 import java.awt.*;
 import javax.swing.*;
+import java.awt.geom.*;
 
 public class GraphicsDisplay extends JPanel {
     private Double[][] graphicsData;
@@ -40,6 +41,7 @@ public class GraphicsDisplay extends JPanel {
         // Шрифт для подписей осей координат
         axisFont = new Font("Serif", Font.BOLD, 15);
     }
+
     // Данный метод вызывается из обработчика элемента меню "Открыть файл с графиком"
     // главного окна приложения в случае успешной загрузки данных
     public void showGraphics(Double[][] graphicsData) {
@@ -48,6 +50,7 @@ public class GraphicsDisplay extends JPanel {
         // Запросить перерисовку компонента, т.е. неявно вызвать paintComponent()
         repaint();
     }
+
     // Методы-модификаторы для изменения параметров отображения графика
     // Изменение любого параметра приводит к перерисовке области
     public void setShowAxis(boolean showAxis) {
@@ -60,4 +63,56 @@ public class GraphicsDisplay extends JPanel {
         repaint();
     }
 
+    public void paintComponent(Graphics g) {
+        /* Шаг 1 - Вызвать метод предка для заливки области цветом заднего фона
+         * Эта функциональность - единственное, что осталось в наследство от
+         * paintComponent класса JPanel
+         */
+        super.paintComponent(g);
+        // Шаг 2 - Если данные графика не загружены (при показе компонента при запуске программы) - ничего не делать
+        if (graphicsData == null || graphicsData.length == 0) return;
+        // Шаг 3 - Определить минимальное и максимальное значения для координат X и Y
+        // Это необходимо для определения области пространства, подлежащей отображению
+        // Е? верхний левый угол это (minX, maxY) - правый нижний это (maxX, minY)
+        minX = graphicsData[0][0];
+        maxX = graphicsData[graphicsData.length - 1][0];
+        minY = graphicsData[0][1];
+        maxY = minY;
+        // Найти минимальное и максимальное значение функции
+        for (int i = 1; i < graphicsData.length; i++) {
+            if (graphicsData[i][1] < minY) {
+                minY = graphicsData[i][1];
+            }
+            if (graphicsData[i][1] > maxY) {
+                maxY = graphicsData[i][1];
+            }
+        }
+        /* Шаг 4 - Определить (исходя из размеров окна) масштабы по осям X и Y - сколько пикселов
+         * приходится на единицу длины по X и по Y
+         */
+        double scaleX = getSize().getWidth() / (maxX - minX);
+        double scaleY = getSize().getHeight() / (maxY - minY);
+        // Шаг 5 - Чтобы изображение было неискаж?нным - масштаб должен быть одинаков
+        // Выбираем за основу минимальный
+        scale = Math.min(scaleX, scaleY);
+        // Шаг 6 - корректировка границ отображаемой области согласно выбранному масштабу
+        if (scale == scaleX) {
+            /* Если за основу был взят масштаб по оси X, значит по оси Y делений меньше,
+             * т.е. подлежащий визуализации диапазон по Y будет меньше высоты окна.
+             * Значит необходимо добавить делений, сделаем это так:
+             * 1) Вычислим, сколько делений влезет по Y при выбранном масштабе - getSize().getHeight()/scale
+             * 2) Вычтем из этого сколько делений требовалось изначально
+             * 3) Набросим по половине недостающего расстояния на maxY и minY
+             */
+            double yIncrement = (getSize().getHeight() / scale - (maxY - minY)) / 2;
+            maxY += yIncrement;
+            minY -= yIncrement;
+        }
+        if (scale == scaleY) {
+            // Если за основу был взят масштаб по оси Y, действовать по аналогии
+            double xIncrement = (getSize().getWidth() / scale - (maxX - minX)) / 2;
+            maxX += xIncrement;
+            minX -= xIncrement;
+        }
+    }
 }
