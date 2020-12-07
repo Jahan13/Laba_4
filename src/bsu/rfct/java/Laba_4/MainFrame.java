2a46bd99cbb06ee53c8835b67a388a9e54658931
Jahan Ovezova <j4hanovezova@yandex.ru>
2020-12-08 00:16:41 +0300

    ADDED:MainFrame fields

--- src/bsu/rfct/java/Laba_4/MainFrame.java
+++ src/bsu/rfct/java/Laba_4/MainFrame.java
@@ -1,6 +1,22 @@
 package bsu.rfct.java.Laba_4;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class MainFrame {
+public class MainFrame extends JFrame {
+    // Начальные размеры окна приложения
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private JFileChooser fileChooser = null;
+    // Пункты меню
+    private JCheckBoxMenuItem showAxisMenuItem;
+    private JCheckBoxMenuItem showMarkersMenuItem;
+    private JCheckBoxMenuItem showIntGraphicsMenuItem;
+    // Компонент-отображатель графика
+    private GraphicsDisplay display = new GraphicsDisplay();
+    // Флаг, указывающий на загруженность данных графика
+    private boolean fileLoaded = false;
+    public static void main(String[] args) {
+
+    }
 
 }
