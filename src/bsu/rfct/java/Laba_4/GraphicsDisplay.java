5fd51f59a367b872cb39079d9ede01eed15cae19
Jahan Ovezova <j4hanovezova@yandex.ru>
2020-12-07 23:24:39 +0300

    ADDED:GraphicsDisplay constructor

--- src/bsu/rfct/java/Laba_4/GraphicsDisplay.java
+++ src/bsu/rfct/java/Laba_4/GraphicsDisplay.java
@@ -20,4 +20,24 @@ public class GraphicsDisplay extends JPanel {
     private BasicStroke axisStroke;
     private BasicStroke markerStroke;
     private BasicStroke graphicsIntStroke;
+    // Различные шрифты отображения надписей
+    private Font axisFont;
+
+    public GraphicsDisplay() {
+        // Цвет заднего фона области отображения - белый
+        setBackground(Color.PINK);
+        // Сконструировать необходимые объекты, используемые в рисовании
+        // Перо для рисования графика
+        graphicsStroke = new BasicStroke(5.0f, BasicStroke.CAP_SQUARE,
+                BasicStroke.JOIN_ROUND, 10.0f, new float[]
+                {30, 10, 20, 10, 10, 10, 20, 10}, 0.0f);
+        // Перо для рисования осей координат
+        axisStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+        // Перо для рисования контуров маркеров
+        markerStroke = new BasicStroke(3.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 45.0f, null, 0.0f);
+        // Шрифт для подписей осей координат
+        axisFont = new Font("Serif", Font.BOLD, 15);
+    }
 }
